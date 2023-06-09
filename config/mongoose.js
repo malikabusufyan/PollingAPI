9433848e2db6c4ef3138b52f176da7e0000ec032
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-06-09 16:51:47 +0530

    submit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,22 @@
 //require the library
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-const mongodbUrl = process.env.MONGODB_URL;
+//connect to the database
+mongoose.connect(
+  "mongodb+srv://malikabusufyan:ld6jeaGzSNJxryle@cluster0.c200owm.mongodb.net/?retryWrites=true&w=majority"
+);
 
-mongoose
-  .connect(mongodbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to Database");
-  })
-  .catch((error) => {
-    console.error("Error connecting to Database", error);
-  });
+//acquire the connection(to check if it's successful)
+const db = mongoose.connection;
+
+//error
+db.on("error", function (err) {
+  console.log(err.message);
+});
+
+//up and running then print the message
+db.once("open", function () {
+  console.log("Successfully connected to the database");
+});
+
+module.exports = db;
