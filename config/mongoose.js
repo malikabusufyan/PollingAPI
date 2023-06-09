edd1632b1e42ce3f4c92a7966ec0e78ced1c39e5
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-06-09 16:48:23 +0530

    Final Submit

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,20 +4,14 @@ require("dotenv").config();
 
 const mongodbUrl = process.env.MONGODB_URL;
 
-//connect to the database
-mongoose.connect(mongodbUrl);
-
-//acquire the connection(to check if it's successful)
-const db = mongoose.connection;
-
-//error
-db.on("error", function (err) {
-  console.log(err.message);
-});
-
-//up and running then print the message
-db.once("open", function () {
-  console.log("Successfully connected to the database");
-});
-
-module.exports = db;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to Database", error);
+  });
